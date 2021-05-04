08e07078c33a4540b1dbe1124c897b968d6b8a4f
Victor <victor@my.home>
2021-05-04 19:45:26 +0300

    Game. CatchIt. WF

---
+++ Game. CatchIt. WF/Game. CatchIt. WF/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Game._CatchIt._WF
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbPlayer = new System.Windows.Forms.Label();
+            this.Time = new System.Windows.Forms.Timer(this.components);
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblCounter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pbPlayer
+            // 
+            this.pbPlayer.BackColor = System.Drawing.Color.Transparent;
+            this.pbPlayer.ForeColor = System.Drawing.Color.Transparent;
+            this.pbPlayer.Image = global::Game._CatchIt._WF.Properties.Resources.person;
+            this.pbPlayer.Location = new System.Drawing.Point(909, 483);
+            this.pbPlayer.Name = "pbPlayer";
+            this.pbPlayer.Size = new System.Drawing.Size(72, 122);
+            this.pbPlayer.TabIndex = 1;
+            // 
+            // Time
+            // 
+            this.Time.Interval = 500;
+            this.Time.Tick += new System.EventHandler(this.Time_Tick);
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.BackColor = System.Drawing.Color.Transparent;
+            this.lblTimer.Font = new System.Drawing.Font("Yu Gothic UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTimer.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblTimer.Location = new System.Drawing.Point(1668, 9);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(185, 64);
+            this.lblTimer.TabIndex = 2;
+            this.lblTimer.Text = "Time : ";
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Transparent;
+            this.btnStart.BackgroundImage = global::Game._CatchIt._WF.Properties.Resources.image_background;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnStart.Font = new System.Drawing.Font("Yu Gothic UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnStart.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnStart.Location = new System.Drawing.Point(688, 358);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(517, 108);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Press To Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblCounter
+            // 
+            this.lblCounter.BackColor = System.Drawing.Color.Transparent;
+            this.lblCounter.Font = new System.Drawing.Font("Yu Gothic UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblCounter.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblCounter.Location = new System.Drawing.Point(1377, 9);
+            this.lblCounter.Name = "lblCounter";
+            this.lblCounter.Size = new System.Drawing.Size(185, 64);
+            this.lblCounter.TabIndex = 4;
+            this.lblCounter.Text = "Counter : 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Game._CatchIt._WF.Properties.Resources.image_background;
+            this.ClientSize = new System.Drawing.Size(1604, 881);
+            this.Controls.Add(this.lblCounter);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.pbPlayer);
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label pbPlayer;
+        private System.Windows.Forms.Timer Time;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblCounter;
+    }
+}
+
---
+++ Game. CatchIt. WF/Game. CatchIt. WF/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game._CatchIt._WF
+{
+    public partial class Form1 : Form
+    {
+        private List<PictureBox> pictureBoxes = new List<PictureBox>();
+        private Bitmap personTexture = Properties.Resources.person;
+        private int score = 0, name = 0;
+        bool isMove = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Full screen
+            this.TopMost = true;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+
+            // Visibility
+            pbPlayer.Enabled = false;
+            pbPlayer.Visible = false;
+            lblCounter.Enabled = false;
+            lblCounter.Visible = false;
+            lblTimer.Enabled = false;
+            lblTimer.Visible = false;
+
+            // Optimize moves of player
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer
+                        | ControlStyles.AllPaintingInWmPaint
+                        | ControlStyles.UserPaint, true);
+            this.UpdateStyles(); // Update them
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Turn on the timer
+            if (!Time.Enabled)
+            {
+                Time.Enabled = true;
+            }
+
+            // Moves of keyboard
+            if (e.KeyCode == Keys.A)
+            {
+                pbPlayer.Location = new Point(pbPlayer.Location.X - 30, pbPlayer.Location.Y);
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                pbPlayer.Location = new Point(pbPlayer.Location.X, pbPlayer.Location.Y - 30);
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                pbPlayer.Location = new Point(pbPlayer.Location.X + 30, pbPlayer.Location.Y);
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                pbPlayer.Location = new Point(pbPlayer.Location.X, pbPlayer.Location.Y + 30);
+            }
+            this.Refresh(); // Invoke method 'Paint()'
+        }
+        private void RndPlaceCoins()
+        {
+            Random random = new Random();
+            PictureBox picture = new PictureBox();
+
+            picture.Name = "picture" + (char)name++;
+            picture.Size = new Size(50, 50);
+            picture.BackgroundImageLayout = ImageLayout.Stretch;
+            picture.BackColor = Color.Transparent;
+
+            int count = random.Next(0, 2);
+            if (count == 0)
+            {
+                picture.BackgroundImage = Image.FromFile("../../../Properties/Image/coffee.png");
+            }
+            else
+            {
+                picture.BackgroundImage = Image.FromFile("../../../Properties/Image/coffee_2.png");
+            }
+            picture.Location = new Point(random.Next(0, 1920), random.Next(0, 1080));
+            pictureBoxes.Add(picture);
+            Controls.Add(picture);
+        }
+        private void VisibilityFinish()
+        {
+            isMove = false;
+            btnStart.Enabled = true;
+            btnStart.Visible = true;
+            pbPlayer.Enabled = false;
+            pbPlayer.Visible = false;
+            lblCounter.Enabled = false;
+            lblCounter.Visible = false;
+            lblTimer.Enabled = false;
+            lblTimer.Visible = false;
+            Time.Enabled = false;
+            Time.Interval = 1000;
+            score = 0;
+            lblCounter.Text = "Score : 0/60";
+            lblTimer.Text = "Time : 0/60";
+        }
+        private void Time_Tick(object sender, EventArgs e)
+        {
+            // Our timer
+
+            // Finish by score
+            if (score == 20)
+            {
+                VisibilityFinish();
+                MessageBox.Show("You are the winner!\n You collect all points {score}/20!", $"The End!", MessageBoxButtons.OK);
+            }
+
+            // Finish by time
+            if (Time.Interval == 60000)
+            {
+                VisibilityFinish();
+                MessageBox.Show("You collect {score}/20 points.", $"The End!", MessageBoxButtons.OK);
+            }
+            lblTimer.Text = $"Time : {Time.Interval / 1000}/60";
+            Time.Interval += 1000;
+            RndPlaceCoins();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // Show and hide different objects to start a game
+            isMove = true;
+            pbPlayer.Enabled = false;
+            lblTimer.Enabled = true;
+            pbPlayer.Visible = false;
+            lblTimer.Visible = true;
+            lblCounter.Enabled = true;
+            lblCounter.Visible = true;
+
+            btnStart.Enabled = false;
+            btnStart.Visible = false;
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (isMove)
+            {
+                // Draw player moves
+                Graphics graphics = e.Graphics;
+                var playerRectangle = new Rectangle(pbPlayer.Location.X, pbPlayer.Location.Y, 100, 100);
+                playerRectangle.Width = 72;
+                playerRectangle.Height = 122;
+                graphics.DrawImage(personTexture, playerRectangle);
+
+                // Collect process of coins
+                CollectCoins(playerRectangle);
+            }
+        }
+        private void CollectCoins(Rectangle playerRectangle)
+        {
+            foreach (var item in pictureBoxes)
+            {
+                // Check the collision of label(our coins) and rectangle of player
+                Rectangle rectangle = item.Bounds;
+                if (playerRectangle.IntersectsWith(rectangle))
+                {
+                    // Remove need coin from 'Form1', 'List<PictureBox>' and plus our 'score'
+                    Controls.Remove(item);
+                    pictureBoxes.Remove(item);
+                    lblCounter.Text = $"Score : {++score}";
+                    break;
+                }
+            }
+        }
+    }
+}
